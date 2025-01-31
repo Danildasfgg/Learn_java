a10d745b604636f83c6d898772efc8bec3065e36
Danildasfgg <danildasfgg@gmail.com>
2025-01-31 22:29:58 +0300

    First lesson
    I did a little work and made a small memo, that is all I do not know what elsoe to say

---
+++ Learn_day_one.java
@@ -0,0 +1,23 @@
+public class Learn_day_one {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I am learning java");
+        System.out.println("It is awensome");
+        System.out.println(10);
+        System.out.println(10 + 50 + 50); //Will output 110, I am not stupid just testing comments
+        final int One_number = 10;
+        int Two_number = 100;
+        Two_number = 10;
+        int Three_number =10;
+        Three_number = 5 * 5;
+        System.out.println(One_number * Two_number * Three_number);
+
+        /*This is long comments, yes I am still test
+         * 
+         * 
+         * 
+         * 
+         * test
+         */
+    }
+}
