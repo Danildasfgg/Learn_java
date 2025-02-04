983f789bbe8f21877a866394d453536e928f7b68
Danildasfgg <danildasfgg@gmail.com>
2025-02-04 22:56:21 +0300

    .

--- Learn_day_three.java
+++ Learn_day_three.java
@@ -1,5 +1,17 @@
 public class Learn_day_three {
     public static void main(String[] args) {
-
+        //Real life project
+        byte items = 50;
+        float constPerItem = 9.99f;
+        float totalConst = items * constPerItem;
+        char iconConst = '$';
+        System.out.println("Total const is " + totalConst + iconConst);
+        //Translation of primitive data types
+        int num = 9;
+        double myDouble = num;
+        System.out.println(myDouble);
+        float num2 = 9.0f;
+        int myInt = (int) num2;
+        System.out.println(myInt);
     }
 }
