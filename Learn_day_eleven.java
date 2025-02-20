0ebfc47100fb20dc9245f549eb63799b24377e9f
Danildasfgg <danildasfgg@gmail.com>
2025-02-20 22:44:42 +0300

    Create Learn_day_eleven.java

---
+++ Learn_day_eleven.java
@@ -0,0 +1,11 @@
+public class Learn_day_eleven {
+    public static void main(String[] args){
+        String[] cars = {"mers", "mazda", "bmw"};
+        for (int i = 0; i < cars.length; i++){
+            System.out.println(cars[i]);
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
